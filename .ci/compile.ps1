bbcbcd397e46f086f524104d4f1401273ef2dce4
MCWertGaming <dleven@protonmail.com>
2021-07-06 20:05:11 +0000

    fixed visual studio not having an "all" target

--- .ci/compile.ps1
+++ .ci/compile.ps1
@@ -1,9 +1,9 @@
 git reset --hard
 git clean -fdx
 cmake -DCMAKE_INSTALL_PREFIX=%cd%\inst .
-cmake --build . --config Debug --target all -j 10
+cmake --build . --config Debug -j 10
 
 git reset --hard
 git clean -fdx
 cmake -DCMAKE_INSTALL_PREFIX=%cd%\inst .
-cmake --build . --config Release --target all -j 10
+cmake --build . --config Release -j 10
