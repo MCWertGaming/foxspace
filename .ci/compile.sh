72541261ab28aa7a9f7018edc7abc8de8346a569
MCWertGaming <dleven@protonmail.com>
2021-07-06 17:51:33 +0000

    compile on 10 threads

--- .ci/compile.sh
+++ .ci/compile.sh
@@ -12,25 +12,25 @@ export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 CLEANUP
 cmake -DCMAKE_INSTALL_PREFIX=./inst .
-cmake --build . --config Debug
+cmake --build . --config Debug --target all -j 10
 
 # compile gcc release
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 CLEANUP
 cmake -DCMAKE_INSTALL_PREFIX=./inst .
-cmake --build . --config Release
+cmake --build . --config Release --target all -j 10
 
 # compile clang debug
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 CLEANUP
 cmake -DCMAKE_INSTALL_PREFIX=./inst .
-cmake --build . --config Debug
+cmake --build . --config Debug --target all -j 10
 
 # compile clang release
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 CLEANUP
 cmake -DCMAKE_INSTALL_PREFIX=./inst .
-cmake --build . --config Release
+cmake --build . --config Release --target all -j 10
